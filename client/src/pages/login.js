faad784ddd528bc57e86419788b3b0d8e7d35875
Hadi Prassetio <hadiprassetio516@gmail.com>
2022-12-09 02:40:49 +0700

    Update Auth

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,10 +4,15 @@ import Form from "react-bootstrap/Form";
 import { Link, useNavigate } from "react-router-dom";
 import { Success, Error } from "../helper/Toast";
 import "../App.css";
+import { API } from "../config/api";
+import { useMutation } from "react-query";
+import { userContext } from "../context/user";
 
 const Login = () => {
   const navigate = useNavigate();
   const [form, setForm] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [state, dispath] = React.useContext(userContext)
 
   const handleChange = (e) => {
     setForm({
@@ -15,6 +20,26 @@ const Login = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleSubmit = useMutation(async(e)=>{
+    try {
+      e.preventDefault();
+      setLoading(true)
+      const response = await API.post("/login", form);
+      Success({ message: 'Login Success!'});
+      setLoading(false);
+      if(response?.status == 200){
+        dispath({
+            type: "Login",
+            payload: response.data.data,
+      });
+    }
+      navigate("/summary")
+    } catch (error) {
+      Error({ message: `Login Failed` });
+      setLoading(false);
+    }
+  })
 
   return (
     <div
@@ -24,7 +49,7 @@ const Login = () => {
         style={{ height: "600px" }}
         className='border border-white col-10 rounded-5 m-auto d-flex align-items-center row'>
         <div className='col-5 mx-auto'>
-          <Form className='text-center w-100'>
+          <Form onSubmit={(e)=>handleSubmit.mutate(e)} className='text-center w-100'>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
               <Form.Control
                 className='rounded-pill border-0 text-light bg-dark py-3'
@@ -47,14 +72,10 @@ const Login = () => {
             </Form.Group>
             {/* <Link className="text-decoration-none text-dark" to="/summary"> */}
             <Button
-              onClick={() => {
-                Success({ message: "Login Success" });
-                navigate("/summary");
-              }}
               variant='danger'
-              // type="submit"
+              type="submit"
               className='w-100 border-0 rounded-pill mb-3 fw-bold  py-3 text-dark'>
-              Log In
+              {loading ? "loading..." : "Log In"}
             </Button>
             {/* </Link> */}
             <Link to='/register' className='text-decoration-none'>
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -4,10 +4,13 @@ import Form from "react-bootstrap/Form";
 import { Link, useNavigate } from "react-router-dom";
 import { Success, Error } from "../helper/Toast";
 import "../App.css";
+import { API } from "../config/api";
+import { useMutation } from "react-query";
 
 const Register = () => {
   const navigate = useNavigate();
   const [form, setForm] = React.useState();
+  const [loading, setLoading] = React.useState(false);
 
   const handleChange = (e) => {
     setForm({
@@ -15,6 +18,20 @@ const Register = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleSubmit = useMutation(async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      await API.post("/register", form);
+      Success({ message: "Success Register" });
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      Error({ message: "Failed Register" });
+      setLoading(false);
+    }
+  });
 
   return (
     <div
@@ -24,7 +41,18 @@ const Register = () => {
         style={{ height: "600px" }}
         className='border border-white col-10 rounded-5 m-auto d-flex align-items-center row'>
         <div className='col-5 mx-auto'>
-          <Form className='text-center'>
+          <Form
+            onSubmit={(e) => handleSubmit.mutate(e)}
+            className='text-center'>
+            <Form.Group className='mb-3' controlId='formBasicName'>
+              <Form.Control
+                className='rounded-pill border-0 text-light bg-dark py-3'
+                type='text'
+                name='name'
+                onChange={handleChange}
+                placeholder='Enter name'
+              />
+            </Form.Group>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
               <Form.Control
                 className='rounded-pill border-0 text-light bg-dark py-3'
@@ -47,14 +75,10 @@ const Register = () => {
             </Form.Group>
             {/* <Link className="text-decoration-none text-dark" to="/summary"> */}
             <Button
-              onClick={() => {
-                Success({ message: "Success Register" });
-                navigate("/");
-              }}
               variant='danger'
-              // type="submit"
+              type='submit'
               className='w-100 border-0 rounded-pill mb-3 fw-bold py-3 text-dark'>
-              Register Account
+              {loading ? "loading..." : "Register Account"}
             </Button>
             {/* </Link> */}
             <Form.Text className='text-muted '>
